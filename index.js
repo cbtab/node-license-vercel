708781f7a99ac5012e0db00fa51e8c5f251bc72e
chalermchat <chalermchat.tab@gmail.com>
2024-02-05 17:52:41 +0700

    i dunno

--- index.js
+++ index.js
@@ -24,41 +24,41 @@ app.get('/test', (req,res)=>{
     res.send("this is api")
 })
 
-const storage = multer.memoryStorage();
-const fileFilter = (req, file, cb) => {
-  const allowedFileTypes = /jpeg|jpg|png/;
-  const extname = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = allowedFileTypes.test(file.mimetype);
+// const storage = multer.memoryStorage();
+// const fileFilter = (req, file, cb) => {
+//   const allowedFileTypes = /jpeg|jpg|png/;
+//   const extname = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
+//   const mimetype = allowedFileTypes.test(file.mimetype);
   
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb(new Error('Only JPEG, JPG, and PNG files are allowed!'));
-  }
-};
+//   if (mimetype && extname) {
+//     return cb(null, true);
+//   } else {
+//     cb(new Error('Only JPEG, JPG, and PNG files are allowed!'));
+//   }
+// };
 
-const upload = multer({ storage: storage, fileFilter: fileFilter });
+// const upload = multer({ storage: storage, fileFilter: fileFilter });
 
-const performOCR = async (imageBuffer) => {
-  const worker = await createWorker('tha');
-  try {
-    await worker.setParameters({
-      tessedit_char_whitelist: 'กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรลวศษสหฬอฮ0123456789',
-      tessedit_min_conf: 10,
-    });
+// const performOCR = async (imageBuffer) => {
+//   const worker = await createWorker('tha');
+//   try {
+//     await worker.setParameters({
+//       tessedit_char_whitelist: 'กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรลวศษสหฬอฮ0123456789',
+//       tessedit_min_conf: 10,
+//     });
 
 
-    const base64Image = imageBuffer.toString('base64');
+//     const base64Image = imageBuffer.toString('base64');
 
-    const { data: { text } } = await worker.recognize(`data:image/jpeg;base64,${base64Image}`);
-    console.log(text);
-    return text;
-  } finally {
-    if (typeof worker.terminate === 'function') {
-      await worker.terminate();
-    }
-  }
-};
+//     const { data: { text } } = await worker.recognize(`data:image/jpeg;base64,${base64Image}`);
+//     console.log(text);
+//     return text;
+//   } finally {
+//     if (typeof worker.terminate === 'function') {
+//       await worker.terminate();
+//     }
+//   }
+// };
 
 
 app.post('/licenseDetector', upload.single('image'), async (req, res) => {
@@ -71,19 +71,6 @@ app.post('/licenseDetector', upload.single('image'), async (req, res) => {
     const base64String = fileBuffer.toString('base64');
 
     const dataUrl = `data:image/jpeg;base64,${base64String}`;
-
-    const response = await axios.post(
-      "https://detect.roboflow.com/license-plate-detection-agqth/3",
-      dataUrl,
-      {
-        params: {
-          api_key: "B8tP2DxAc04VzSzBvu6J"
-        },
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        }
-      }
-    );
 
     // const prediction = response.data.predictions[0];
     // const imageWidth = response.data.image.width;
