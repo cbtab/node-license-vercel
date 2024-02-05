ed8b84c0614782fd511d1cf36db7ed2e0bda2af9
chalermchat <chalermchat.tab@gmail.com>
2024-02-05 18:03:49 +0700

    77

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const path = require('path');
+
 const cors = require('cors');
-const multer = require('multer');
+
 const { createWorker } = require('tesseract.js');
 
 
@@ -21,21 +21,6 @@ app.get('/', (req,res)=>{
 app.get('/test', (req,res)=>{
     res.send("this is api")
 })
-
-const storage = multer.memoryStorage();
-const fileFilter = (req, file, cb) => {
-  const allowedFileTypes = /jpeg|jpg|png/;
-  const extname = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = allowedFileTypes.test(file.mimetype);
-  
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb(new Error('Only JPEG, JPG, and PNG files are allowed!'));
-  }
-};
-
-const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 
 const performOCR = async (imageBuffer) => {
@@ -60,45 +45,24 @@ const performOCR = async (imageBuffer) => {
 };
 
 
-app.post('/licenseDetector', upload.single('image'), async (req, res) => {
-  try {
-    if (!req.file) {
-      throw new Error('No file uploaded!');
+app.post('/licenseDetector', async (req, res) => {
+    try {
+      
+        const base64String = req.body.image;
+
+        const fileBuffer = Buffer.from(base64String, 'base64');
+
+
+        const ocrResult = await performOCR(fileBuffer);
+
+        res.json({ license_plate_text: ocrResult });
+        return;
+    } catch (error) {
+        console.error("Error processing image:", error);
+        res.status(500).json({ error: error.message || "Internal Server Error" });
+        return;
     }
-
-
-
-
-
-
-    // const prediction = response.data.predictions[0];
-    // const imageWidth = response.data.image.width;
-    // const imageHeight = response.data.image.height;
-
-    // console.log(response.data);
-    // console.log("Image received from client");
-
-    // const { x, y, width, height } = prediction;
-
-    // const left = Math.max(0, Math.floor(x - width * 0.5));
-    // const top = Math.max(0, Math.floor(y - height * 0.5));
-    // const cropWidth = Math.min(Math.floor(width), imageWidth - left);
-    // const cropHeight = Math.min(Math.floor(height), imageHeight - top);
-
-    // const croppedImageBuffer = await sharp(fileBuffer)
-    //   .extract({ left, top, width: cropWidth, height: cropHeight })
-    //   .toBuffer();
-    // const ocrResult = await performOCR(croppedImageBuffer);
-
-    res.json({ license_plate_text: "hello", result: "hello"});
-    return;
-  } catch (error) {
-    console.error("Error processing image:", error);
-    res.status(500).json({ error: error.message || "Internal Server Error" });
-    return;
-  }
 });
-
 
 
 module.exports = app;
