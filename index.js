a368a9cfd326f75cd0903ea11d869043a72b6233
chalermchat <chalermchat.tab@gmail.com>
2024-02-05 17:56:36 +0700

    3

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-
-const { createWorker } = require('tesseract.js');
+const multer = require('multer');
 const cors = require('cors');
-
+const path = require('path');
+const { createWorker } = require('tesseract.js');
 
 
 const app = express()
@@ -22,41 +22,41 @@ app.get('/test', (req,res)=>{
     res.send("this is api")
 })
 
-// const storage = multer.memoryStorage();
-// const fileFilter = (req, file, cb) => {
-//   const allowedFileTypes = /jpeg|jpg|png/;
-//   const extname = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
-//   const mimetype = allowedFileTypes.test(file.mimetype);
+const storage = multer.memoryStorage();
+const fileFilter = (req, file, cb) => {
+  const allowedFileTypes = /jpeg|jpg|png/;
+  const extname = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = allowedFileTypes.test(file.mimetype);
   
-//   if (mimetype && extname) {
-//     return cb(null, true);
-//   } else {
-//     cb(new Error('Only JPEG, JPG, and PNG files are allowed!'));
-//   }
-// };
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb(new Error('Only JPEG, JPG, and PNG files are allowed!'));
+  }
+};
 
-// const upload = multer({ storage: storage, fileFilter: fileFilter });
+const upload = multer({ storage: storage, fileFilter: fileFilter });
 
-// const performOCR = async (imageBuffer) => {
-//   const worker = await createWorker('tha');
-//   try {
-//     await worker.setParameters({
-//       tessedit_char_whitelist: 'กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรลวศษสหฬอฮ0123456789',
-//       tessedit_min_conf: 10,
-//     });
+const performOCR = async (imageBuffer) => {
+  const worker = await createWorker('tha');
+  try {
+    await worker.setParameters({
+      tessedit_char_whitelist: 'กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรลวศษสหฬอฮ0123456789',
+      tessedit_min_conf: 10,
+    });
 
 
-//     const base64Image = imageBuffer.toString('base64');
+    const base64Image = imageBuffer.toString('base64');
 
-//     const { data: { text } } = await worker.recognize(`data:image/jpeg;base64,${base64Image}`);
-//     console.log(text);
-//     return text;
-//   } finally {
-//     if (typeof worker.terminate === 'function') {
-//       await worker.terminate();
-//     }
-//   }
-// };
+    const { data: { text } } = await worker.recognize(`data:image/jpeg;base64,${base64Image}`);
+    console.log(text);
+    return text;
+  } finally {
+    if (typeof worker.terminate === 'function') {
+      await worker.terminate();
+    }
+  }
+};
 
 
 app.post('/licenseDetector', async (req, res) => {
